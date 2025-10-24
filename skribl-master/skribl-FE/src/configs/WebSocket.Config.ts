bfa2a075b600009750b6038f0435fbc3838b19b4
Ranveergit <141477576+Ranveergit@users.noreply.github.com>
2025-10-24 15:30:43 +0530

    Update WebSocket.Config.ts

--- skribl-master/skribl-FE/src/configs/WebSocket.Config.ts
+++ skribl-master/skribl-FE/src/configs/WebSocket.Config.ts
@@ -1,3 +1,3 @@
 export const WebSocketConfig = {
-  BASE_URL: import.meta.env.VITE_WS_HOST ?? "http://localhost:4000",
+  BASE_URL: import.meta.env.VITE_WS_HOST ?? "https://skrible-a-multiplayer-sketch-game-6.onrender.com",
 };
